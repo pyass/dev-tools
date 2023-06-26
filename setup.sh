617252294b44c16a84a7f5ae1a3ec320eb544c5e
pyass <pyassistant00@gmail.com>
2023-06-26 19:33:35 +0000

    added fzf to setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 LOCATION=$(dirname "$(readlink -f "$0")")
 cp ${LOCATION}/.{vimrc,bash*,tmux*} $HOME/
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install 
 source ~/.bashrc
